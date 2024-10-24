67c47ddef77f7a77d3d73a1660b7cb997d79e6cc
Manpreet-s1ngh <64644727+Manpreet-s1ngh@users.noreply.github.com>
2024-10-23 22:53:25 -0400

    Update sanityClient.js

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -9,7 +9,7 @@ const client = sanityClient({
   dataset: "production", // Replace with your dataset name
   apiVersion: "2022-03-07", // Use the current date for API version
   useCdn: false, // Set to false if you need fresh data
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: "skC5xXJw0H6wO9KYhpccyAvHdBA5rFGEoE8E6oAVeJxUSUi5xu8ObSdDHG1BWBHwf6KdksOBgNtCqePZtjoRMyCwjD3zfwXl2imzRkjLklwlUY6vgdAau2TWer7A8fAv2zeS9Gg1Nt9pm6lKoEnCeyKk5vMeOAhbetSFlKKZy8YVQ5o7hjhx",
 });
 
 // Initialize image URL builder
